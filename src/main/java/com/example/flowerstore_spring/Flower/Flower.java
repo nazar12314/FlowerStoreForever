b7938597ae67b8219d64d793f481288810e05b50
nazar12314 <kononenkonazar47@gmail.com>
2022-11-13 11:01:09 +0200

    uploaded all

--- src/main/java/com/example/flowerstore_spring/Flower/Flower.java
+++ src/main/java/com/example/flowerstore_spring/Flower/Flower.java
@@ -5,12 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.Table;
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
+import javax.persistence.*;
 
 @NoArgsConstructor
 @Getter
